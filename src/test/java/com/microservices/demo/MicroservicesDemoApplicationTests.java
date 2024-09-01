ae376f796b191871f029ca1faadb436a0dc0c396
Ievgen Makhnonosov <Ievgen.Makhnonosov@ibm.com>
2024-09-01 20:08:37 +0300

    initial commit

---
+++ src/test/java/com/microservices/demo/MicroservicesDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.microservices.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
