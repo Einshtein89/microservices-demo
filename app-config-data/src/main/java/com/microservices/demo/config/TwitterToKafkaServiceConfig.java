04ce2c111bdb0f67265e860a32da87604f0b32c6
Ievgen Makhnonosov <Ievgen.Makhnonosov@ibm.com>
2024-09-04 19:51:34 +0300

    1. Created twitter service to emit stream of data. 2. Created kafka cluster and producer to transform data into avro and store as events. 3. Created external configs and store in remote git repo.

---
+++ app-config-data/src/main/java/com/microservices/demo/config/TwitterToKafkaServiceConfig.java
@@ -0,0 +1,23 @@
+package com.microservices.demo.config;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "twitter-to-kafka-service")
+public class TwitterToKafkaServiceConfig
+{
+  private List<String> twitterKeywords;
+  private String twitterV2BaseUrl;
+  private String twitterV2RulesBaseUrl;
+  private String twitterV2BearerToken;
+  private Boolean enableMockTweets;
+  private Long mockSleepMs;
+  private Integer mockMinTweetLength;
+  private Integer mockMaxTweetLength;
+}
