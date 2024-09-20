5fc04fc2169d31ffb36fa26ff43d3a67a26be4ec
Ievgen Makhnonosov <Ievgen.Makhnonosov@ibm.com>
2024-09-20 16:59:51 +0300

    added keycloak and security config changes to query service

--- app-config-data/src/main/java/com/microservices/demo/config/ElasticQueryServiceConfigData.java
+++ app-config-data/src/main/java/com/microservices/demo/config/ElasticQueryServiceConfigData.java
@@ -11,5 +11,6 @@ import lombok.Data;
 public class ElasticQueryServiceConfigData
 {
   private String version;
+  private String customAudience;
   private Long backPressureDelayMs;
 }
--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
+@PreAuthorize("isAuthenticated()")
 @RestController
 @RequestMapping(value = "/documents", produces = "application/vnd.api.v1+json")
 public class ElasticDocumentController
@@ -96,6 +98,7 @@ public class ElasticDocumentController
     return ResponseEntity.ok(getV2Model(response));
   }
 
+  @PreAuthorize("hasRole('APP_USER_ROLE') || hasAuthority('SCOPE_APP_USER_ROLE')")
   @Operation(summary = "Get elastic document by text")
   @ApiResponses(value = {
       @ApiResponse(responseCode = "200", description = "success", content = {
